0a552e205e3f8f64a05b644f3fed89ff89e97681
indexer <yemonkyaw007@gmail.com>
2016-04-10 19:34:29 +0630

    just fun for thingyan

---
+++ recommendation_data.py
@@ -0,0 +1,33 @@
+# Collabrative Filtering data set taken from Collective Intelligence book.
+
+
+dataset={
+			'ye lin aung':{                  'Room': 4.5, 
+							'The Phoenix Project: A Novel About IT DevOps and Helping Your Business Win': 4.2,
+							'When Breath Becomes Air': 3.0, 
+							'After Dark': 3.5, 
+							'Sputnik Sweetheart': 4.0,
+							'Birthday Stories: Selected and Introduced by Haruki Murakami': 3.0},
+			'Li ja Li': {                   'When Breath Becomes Air': 3.0, 
+                                                        'Room': 4.5,
+                                                         'Dark Places':4.0,
+							'Zen Pencils: Cartoon Quotes from Inspirational Folks': 1.5,
+							 'The Wise Mans Fear (The Kingkiller Chronicle, #2)': 5.0, 
+							'The Non-Designer Design Book': 3.5},
+			'Ye Myat Thu': {                 'The Lies of Locke Lamora (Gentleman Bastard, #1)': 5.0, 
+                                                         'Room': 4.5,
+							'Foundling (Monster Blood Tattoo, #1)': 3.8,
+							'TThe Curious Case of Benjamin Button': 4.5, 
+							'Harry Potter and the Deathly Hallows (Harry Potter, #7)': 4.0 },
+							
+			'Poe Poe Myint Shwe': {          'Lady in the Water': 3.0, 
+							'Startup Focus': 3.0,
+							'Just My Luck': 2.0, 
+                                                         'Room': 4.5,
+							'Zen Pencils: Cartoon Quotes from Inspirational Folks': 3.0, 
+							'The Piano Tuner': 3.0,
+							'Eragon, Eldest & Brisingr (Inheritance, #1-3)': 4.0},
+			
+			'Misa ki Moe': {                'The Non-Designer Design Book':4.5,
+					                'Persepolis: The Story of a Childhood (Persepolis, #1-2)': 3.0,
+					                'Only Time Will Tell (The Clifton Chronicles, #1)':4.0}}
